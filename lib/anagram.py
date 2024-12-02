b2376ab5a1a99fa4fce2ad01323c90fe51180037
kamalabdi8 <kamalabdi042@gmail.com>
2024-12-02 14:19:43 +0300

    first commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = ''.join(sorted(word))
+
+    def match(self, possible_anagrams):
+        matches = []
+        for candidate in possible_anagrams:
+            if ''.join(sorted(candidate)) == self.sorted_word:
+                matches.append(candidate)
+        return matches
+
+
+    
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
